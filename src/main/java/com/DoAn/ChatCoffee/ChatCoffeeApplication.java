5fbeb10f18f9a7618877d9523dedd37d38b93ef5
TranQuangVi <112139716+TranQuangVi@users.noreply.github.com>
2023-05-13 14:44:28 +0700

    commit

--- src/main/java/com/DoAn/ChatCoffee/ChatCoffeeApplication.java
+++ src/main/java/com/DoAn/ChatCoffee/ChatCoffeeApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChatCoffeeApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(ChatCoffeeApplication.class, args);
 	}

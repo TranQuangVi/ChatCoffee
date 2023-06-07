4cc5657373a921e48a9fe0dfda94e2ea25a0ff30
Sabo1901 <hieugia777@gmail.com>
2023-06-07 20:19:42 +0700

    Hieu-Up
    Home

--- src/main/java/com/DoAn/ChatCoffee/controller/HomeController.java
+++ src/main/java/com/DoAn/ChatCoffee/controller/HomeController.java
@@ -1,14 +1,21 @@
 package com.DoAn.ChatCoffee.controller;
 
+import com.DoAn.ChatCoffee.service.SanPhamService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/")
 public class HomeController {
+    @Autowired
+    private SanPhamService sanPhamService;
     @GetMapping
-    public String homePage(){
+    public String homePage(Model model){
+
+        model.addAttribute("listProducts", sanPhamService.getAllProduct());
         return "home/index";
     }
 }
